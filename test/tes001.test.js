81ba4fb502de6eb9f9d1037f0a5720792ecaffb7
Tugdual Grall <tgrall@github.com>
2022-11-04 10:12:15 +0200

    Update tes001.test.js

--- test/tes001.test.js
+++ test/tes001.test.js
@@ -1,4 +1,5 @@
 var assert = require('assert');
+const x = "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";
 describe('Array', function() {
   describe('#indexOf()', function() {
     it('should return -1 when the value is not present', function() {
